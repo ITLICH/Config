bc6f6fb675184a2b0d049afd4100458cd9a49548
lch <751235739@qq.com>
2020-09-23 22:32:56 +0800

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Config
+{
+    partial class ConfigManager
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreatTemplate = new System.Windows.Forms.Button();
+            this.SelectProject = new System.Windows.Forms.Button();
+            this.pathLable = new System.Windows.Forms.Label();
+            this.CreatBinaryBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CreatTemplate
+            // 
+            this.CreatTemplate.Location = new System.Drawing.Point(80, 319);
+            this.CreatTemplate.Name = "CreatTemplate";
+            this.CreatTemplate.Size = new System.Drawing.Size(75, 23);
+            this.CreatTemplate.TabIndex = 0;
+            this.CreatTemplate.Text = "生成模板";
+            this.CreatTemplate.UseVisualStyleBackColor = true;
+            this.CreatTemplate.Click += new System.EventHandler(this.CreatTemplate_Click);
+            // 
+            // SelectProject
+            // 
+            this.SelectProject.Location = new System.Drawing.Point(80, 104);
+            this.SelectProject.Name = "SelectProject";
+            this.SelectProject.Size = new System.Drawing.Size(131, 23);
+            this.SelectProject.TabIndex = 0;
+            this.SelectProject.Text = "选择项目目录";
+            this.SelectProject.Click += new System.EventHandler(this.SelectProject_Click);
+            // 
+            // pathLable
+            // 
+            this.pathLable.AutoSize = true;
+            this.pathLable.Location = new System.Drawing.Point(259, 109);
+            this.pathLable.Name = "pathLable";
+            this.pathLable.Size = new System.Drawing.Size(131, 12);
+            this.pathLable.TabIndex = 1;
+            this.pathLable.Text = global::Config.Properties.Settings.Default.PATH;
+            // 
+            // CreatBinaryBtn
+            // 
+            this.CreatBinaryBtn.Location = new System.Drawing.Point(240, 319);
+            this.CreatBinaryBtn.Name = "CreatBinaryBtn";
+            this.CreatBinaryBtn.Size = new System.Drawing.Size(150, 23);
+            this.CreatBinaryBtn.TabIndex = 2;
+            this.CreatBinaryBtn.Text = "创建c#结构和二进制文件";
+            this.CreatBinaryBtn.UseVisualStyleBackColor = true;
+            this.CreatBinaryBtn.Click += new System.EventHandler(this.CreatBinaryBtn_Click);
+            // 
+            // ConfigManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CreatBinaryBtn);
+            this.Controls.Add(this.pathLable);
+            this.Controls.Add(this.SelectProject);
+            this.Controls.Add(this.CreatTemplate);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "ConfigManager";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ConfigManager_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreatTemplate;
+        private System.Windows.Forms.Button SelectProject;
+        private System.Windows.Forms.Label pathLable;
+        private System.Windows.Forms.Button CreatBinaryBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Core;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Config
+{
+    public partial class ConfigManager : Form
+    {
+        public ConfigManager()
+        {
+            InitializeComponent();
+        }
+        string defaultPath = string.Empty;
+
+        private void CreatTemplate_Click(object sender, EventArgs e)
+        {
+
+
+            if (string.IsNullOrEmpty(pathLable.Text))
+            {
+                MessageBox.Show("请先选择项目路径");
+            }
+            else
+            {
+
+            }
+
+            CreatExcelTemolate();
+        }
+
+        //private string projectPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        private bool CreatExcelTemolate()
+        {
+            bool result = false;
+            try
+            {
+                System.Reflection.Missing missing = System.Reflection.Missing.Value;
+                Excel.Application app = new Excel.ApplicationClass();
+                Excel.Workbooks workbooks = app.Workbooks;
+                Excel.Workbook workbook = workbooks.Add(missing);
+                Excel.Worksheet worksheet = workbook.Sheets[1] as Excel.Worksheet;
+                worksheet.Name = "Only This";
+
+                worksheet.Cells[1, 1] = "类型";
+                worksheet.Cells[1, 2] = "int";
+
+                worksheet.Cells[2, 1] = "描述";
+                worksheet.Cells[2, 2] = "对象的id";
+
+                worksheet.Cells[3, 1] = "字段名";
+                worksheet.Cells[3, 2] = "id";
+
+                string excelPathRoot = pathLable.Text + "/GameTable";
+                if (Directory.Exists(excelPathRoot) == false)
+                {
+                    Directory.CreateDirectory(excelPathRoot);
+                }
+
+                worksheet.SaveAs(excelPathRoot + "/默认模板.xlsx");
+                workbook.Close();
+                app.Quit();
+
+                //打开文件夹
+                System.Diagnostics.Process.Start("explorer.exe", excelPathRoot + @"\");
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"创建错误：{e.Message}");
+            }
+
+
+
+            return result;
+        }
+
+        private void SelectProject_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择项目目录";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                pathLable.Text = dialog.SelectedPath;
+                Config.Properties.Settings.Default.PATH = pathLable.Text;
+            }
+        }
+
+        private void ConfigManager_Load(object sender, EventArgs e)
+        {
+            Config.Properties.Settings.Default.Save();
+        }
+
+        private void CreatBinaryBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// 生成c#结构类
+        /// </summary>
+        private void CreatCFile()
+        { 
+        
+        }
+
+        /// <summary>
+        /// 生成二进制文件
+        /// </summary>
+        private void CreatBinaryFile()
+        {
+        
+        }
+
+
+    }
+
+}
